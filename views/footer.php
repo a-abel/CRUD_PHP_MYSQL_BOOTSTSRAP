1e7f1a57a15db8f2d9327555660b2361d1e10c8d
andresm10 <wilan10@outlook.com>
2018-09-06 22:28:23 -0500

    Add files to CRUD in PHP 7, Mysql y Bootstrap 4
    Add files to CRUD in PHP 7, Mysql y Bootstrap 4

---
+++ views/footer.php
@@ -0,0 +1,15 @@
+
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col text-center">
+			<p>Desarrollado por Andr&eacute;s M.</p>
+			<p>Visita <a href="http://codigoweblibre.blogspot.com">codigoweblibre.blogspot.com</a></p>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="./misc/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="./misc/bootstrap/bootstrap.min.js"></script>
+
+</body>
+</html>
